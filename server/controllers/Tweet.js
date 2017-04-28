0401c3939977420fedf2a5e68c0f0c8548e09433
Clayton <cscavone96@gmail.com>
2017-04-28 03:17:52 -0400

    Tweet files

---
+++ server/controllers/Tweet.js
@@ -0,0 +1,243 @@
+const models = require('../models');
+
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+
+const Tweet = models.Tweet;
+
+const sendAjax = (request, response, url) => {
+  const xhr = new XMLHttpRequest();
+  // console.log(url);
+  // xhr.onload = () => handleResponse(request, response, xhr);
+  xhr.open('GET', url, false);
+  xhr.responseType = 'document';
+  xhr.send();
+  return xhr.responseText;
+};
+
+const getBadTweetCount = (tc, tweet) => Tweet.TweetModel.findById(
+tweet.id, 'income textContents createdData', (err, dcs) => {
+  const tcc = tc;
+  if (dcs.income === 1) {
+    return tcc.count++;
+  }
+  return null;
+});
+
+const getLowTweetCount = (tc, tweet) => Tweet.TweetModel.findById(
+tweet.id, 'income textContents createdData', (err, dcs) => {
+  const tcc = tc;
+  if (dcs.income === 2) {
+    return tcc.count++;
+  }
+  return null;
+});
+
+const makeBadTweet = (req, res) => models.Tweet.TweetModel.findByOwner(
+  req.session.account._id, (er, docos) => {
+    const cost = 25;
+    if (er) {
+      console.log(er);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+    const tweets = docos;
+    const tweetCount = { count: 1 };
+    if (tweets.length === 0) {
+      return models.Account.AccountModel.findOne({
+        username: req.session.account.username }, (err, docs) => {
+        const impressions = docs.impressions;
+        if (parseInt(impressions, 10) < (cost * tweetCount.count * tweetCount.count)) {
+          return res.status(400).json({ error: 'Impressions are required' });
+        }
+
+        const query = { username: req.session.account.username };
+        const newCredits = { $inc: { impressions: -(cost * tweetCount.count * tweetCount.count) } };
+        let tweetText = 'test tweet plz ignore';
+        tweetText = sendAjax(req, res, 'https://api.randomuser.me/');
+        const jsonTweet = JSON.parse(tweetText);
+        const tweetEndings = [' dude im on this website now', ' promote this account',
+          ' follow if u know whats good for you', ' what up fam', ' funny seeing you on here',
+          ' are you still in school',
+          ' i thought you were in prison', ' wanna get food sometime', ' r u single??',
+          ' who are you again?', ' please follow me',
+          ' follow me back', ' please follow.', ' follow me I post bad content',
+          ' if you are under 18 please dont follow', ' how does this website work',
+          " i'm back", ' lol', ' i love you', ' please someone answer me',
+          " if u could just click the 'follow' button please", ' pls respond'];
+        const tweetEnding = tweetEndings[Math.floor(Math.random() * tweetEndings.length)];
+        tweetText = `@${jsonTweet.results[0].login.username}${tweetEnding}`;
+        return models.Account.AccountModel.update(query, newCredits, () => {
+          const tweetData = {
+            owner: req.session.account._id,
+            income: 1,
+            textContents: tweetText,
+          };
+
+          const newTweet = new Tweet.TweetModel(tweetData);
+
+          const tweetPromise = newTweet.save();
+
+          tweetPromise.then(() => res.status(200).json({ message: 'Tweet created.' }));
+
+          tweetPromise.catch((error) => {
+            console.log(error);
+            if (err.code === 11000) {
+              return res.status(400).json({ error: 'Tweet already exists.' });
+            }
+            return res.status(400).json({ error: 'An error occured.' });
+          });
+          return tweetPromise;
+        });
+      });
+    }
+    for (let i = 0; i < tweets.length; i++) {
+      getBadTweetCount(tweetCount, tweets[i]);
+      if (i === tweets.length - 1) {
+        models.Account.AccountModel.findOne({
+          username: req.session.account.username }, (err, docs) => {
+          const impressions = docs.impressions;
+          // console.log(tweetCount);
+          if (parseInt(impressions, 10) < (cost * tweetCount.count * tweetCount.count)) {
+            return res.status(400).json({ error: 'Impressions are required' });
+          }
+
+          const query = { username: req.session.account.username };
+          const newCredits =
+          { $inc: { impressions: -(cost * tweetCount.count * tweetCount.count) } };
+          let tweetText = 'test tweet plz ignore';
+          tweetText = sendAjax(req, res, 'https://api.randomuser.me/');
+          const jsonTweet = JSON.parse(tweetText);
+          const tweetEndings = [' dude im on this website now', ' promote this account',
+            ' follow if u know whats good for you', ' what up fam',
+            ' funny seeing you on here', ' are you still in school',
+            ' i thought you were in prison', ' wanna get food sometime',
+            ' r u single??', ' who are you again?', ' please follow me',
+            ' follow me back', ' please follow.', ' follow me I post bad content',
+            ' if you are under 18 please dont follow', ' how does this website work',
+            " i'm back", ' lol', ' i love you', ' please someone answer me',
+            " if u could just click the 'follow' button please",
+            ' pls respond'];
+          tweetText =
+          `@${jsonTweet.results[0].login.username}${tweetEndings
+          [Math.floor(Math.random() * tweetEndings.length)]}`;
+          return models.Account.AccountModel.update(query, newCredits, () => {
+            const tweetData = {
+              owner: req.session.account._id,
+              income: 1,
+              textContents: tweetText,
+            };
+
+            const newTweet = new Tweet.TweetModel(tweetData);
+
+            const tweetPromise = newTweet.save();
+
+            tweetPromise.then(() => res.status(200).json({ message: 'Tweet created.' }));
+
+            tweetPromise.catch((error) => {
+              console.log(error);
+              if (err.code === 11000) {
+                return res.status(400).json({ error: 'Tweet already exists.' });
+              }
+              return res.status(400).json({ error: 'An error occured.' });
+            });
+            return tweetPromise;
+          });
+        });
+      }
+    }
+    return null;
+  });
+
+const makeLowTweet = (req, res) => models.Tweet.TweetModel.findByOwner(
+  req.session.account._id, (er, docos) => {
+    const cost = 100;
+    if (er) {
+      console.log(er);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+    const tweets = docos;
+    const tweetCount = { count: 1 };
+    if (tweets.length === 0) {
+      return models.Account.AccountModel.findOne({
+        username: req.session.account.username }, (err, docs) => {
+        const impressions = docs.impressions;
+        if (parseInt(impressions, 10) < (cost * tweetCount * tweetCount)) {
+          return res.status(400).json({ error: 'Impressions are required' });
+        }
+
+        const query = { username: req.session.account.username };
+        const newCredits = { $inc: { impressions: -(cost * tweetCount * tweetCount) } };
+        let tweetText = 'test tweet plz ignore';
+        tweetText = sendAjax(req, res, 'http://catfacts-api.appspot.com/api/facts');
+        const jsonTweet = JSON.parse(tweetText);
+        tweetText = jsonTweet.facts[0];
+        return models.Account.AccountModel.update(query, newCredits, () => {
+          const tweetData = {
+            owner: req.session.account._id,
+            income: 2,
+            textContents: tweetText,
+          };
+
+          const newTweet = new Tweet.TweetModel(tweetData);
+
+          const tweetPromise = newTweet.save();
+
+          tweetPromise.then(() => res.status(200).json({ message: 'Tweet created.' }));
+
+          tweetPromise.catch((error) => {
+            console.log(error);
+            if (err.code === 11000) {
+              return res.status(400).json({ error: 'Tweet already exists.' });
+            }
+            return res.status(400).json({ error: 'An error occured.' });
+          });
+          return tweetPromise;
+        });
+      });
+    }
+    for (let i = 0; i < tweets.length; i++) {
+      getLowTweetCount(tweetCount, tweets[i]);
+      if (i === tweets.length - 1) {
+        return models.Account.AccountModel.findOne({
+          username: req.session.account.username }, (err, docs) => {
+          const impressions = docs.impressions;
+          if (parseInt(impressions, 10) < (cost * tweetCount * tweetCount)) {
+            return res.status(400).json({ error: 'Impressions are required' });
+          }
+
+          const query = { username: req.session.account.username };
+          const newCredits = { $inc: { impressions: -(cost * tweetCount * tweetCount) } };
+          let tweetText = 'test tweet plz ignore';
+          tweetText = sendAjax(req, res, 'http://catfacts-api.appspot.com/api/facts');
+          const jsonTweet = JSON.parse(tweetText);
+          tweetText = jsonTweet.facts[0];
+          return models.Account.AccountModel.update(query, newCredits, () => {
+            const tweetData = {
+              owner: req.session.account._id,
+              income: 2,
+              textContents: tweetText,
+            };
+
+            const newTweet = new Tweet.TweetModel(tweetData);
+
+            const tweetPromise = newTweet.save();
+
+            tweetPromise.then(() => res.status(200).json({ message: 'Tweet created.' }));
+
+            tweetPromise.catch((error) => {
+              console.log(error);
+              if (err.code === 11000) {
+                return res.status(400).json({ error: 'Tweet already exists.' });
+              }
+              return res.status(400).json({ error: 'An error occured.' });
+            });
+            return tweetPromise;
+          });
+        });
+      }
+    }
+    return null;
+  });
+
+module.exports.makeBad = makeBadTweet;
+module.exports.makeLow = makeLowTweet;
---
+++ server/models/Tweet.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let TweetModel = {};
+
+const convertId = mongoose.Types.ObjectId;
+
+
+const TweetSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+
+  income: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  textContents: {
+    type: String,
+    required: true,
+    trim: true,
+    default: 'Default text',
+  },
+
+  createdData: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+TweetSchema.statics.toAPI = (doc) => ({
+  income: doc.income,
+  text: doc.text,
+});
+
+
+TweetSchema.statics.findByOwner = (ownerId, callback) => {
+  const search = {
+    owner: convertId(ownerId),
+  };
+
+  return TweetModel.find(search).select('impressions').exec(callback);
+};
+
+TweetModel = mongoose.model('Tweet', TweetSchema);
+module.exports.TweetModel = TweetModel;
+module.exports.TweetSchema = TweetSchema;
